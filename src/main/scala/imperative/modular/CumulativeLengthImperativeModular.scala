0d91976d0b121764f7ba2e464e59981ebe2ffbb6
Irinel Bandas <ibandas@luc.edu>
2018-02-22 19:58:03 -0600

    Made the values double for min, max, and avg for stats

--- src/main/scala/imperative/modular/CumulativeLengthImperativeModular.scala
+++ src/main/scala/imperative/modular/CumulativeLengthImperativeModular.scala
@@ -13,35 +13,40 @@ trait AccumulateLength extends Task with Output[(Int, Int)] {
     var windowQueue = Queue[Int]()
     val windowSizeMax = windowSizes.max
 
-    for (line <- input) { //Loops through each number stream
-      windowQueue.enqueue(line)
-      if (windowQueue.length > windowSizeMax) { //If the queue is at max length, dequeue so the new value will be enqueued
-        windowQueue.dequeue()
-      }
-      counter = counter + 1
-      print(line + ", " + counter + ", ") //First two numbers are always the input number and counter
-      for (windowSize <- windowSizes) { //Uses the queue for each window to get stats
-        //If there is enough numbers in queue, do stats
-        if (windowQueue.length >= windowSize) {
-          val stats = movingAverage(windowQueue, windowSize)
-          print(stats._1 + ", " + stats._2 + ", " + stats._3 + ", ")
-        } else { //If there is not enough numbers in queue, print question marks
-          print("?, ?, ? | ")
+    if (!windowSizes.isEmpty) {
+      for (line <- input) { //Loops through each number stream
+
+        windowQueue.enqueue(line)
+        if (windowQueue.length > windowSizeMax) { //If the queue is at max length, dequeue so the new value will be enqueued
+          windowQueue.dequeue()
         }
+        counter = counter + 1
+        print(line + ", " + counter + ", ") //First two numbers are always the input number and counter
+        for (windowSize <- windowSizes) { //Uses the queue for each window to get stats
+          //If there is enough numbers in queue, do stats
+          if (windowQueue.length >= windowSize) {
+            val stats = movingAverage(windowQueue, windowSize)
+            print(stats._1 + ", " + stats._2 + ", " + stats._3 + ", ")
+          } else { //If there is not enough numbers in queue, print question marks
+            print("?, ?, ? | ")
+          }
+        }
+        println(" ")
+        //length += line.length
+        //doOutput((line, length))
       }
-      println(" ")
-      //length += line.length
-      //doOutput((line, length))
+    } else {
+      println("Not enough windows")
     }
   }
 
   def movingAverage(inputQueue: Queue[Int], divisible: Int) = {
     val windowArray = inputQueue.takeRight(divisible)
     val sum = windowArray.sum
-    val min = windowArray.min
-    val max = windowArray.max
-    val avg = sum / divisible
-    (min, avg, max)
+    val min = windowArray.min.doubleValue()
+    val max = windowArray.max.doubleValue()
+    val avg = sum.doubleValue() / divisible.doubleValue()
+    (min, avg.doubleValue(), max)
   }
 }
 
