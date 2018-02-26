afbf08963d406643671cefacbce449f39c2103e4
Irinel Bandas <ibandas@luc.edu>
2018-02-25 18:06:35 -0600

    Trying to figure out how to make the test cases

--- src/test/scala/imperative/modular/BenchmarkCumulativeLengthImperative.scala
+++ src/test/scala/imperative/modular/BenchmarkCumulativeLengthImperative.scala
@@ -6,15 +6,17 @@ object BenchmarkCumulativeLengthImperative extends Bench.LocalTime {
 
   val sut = new AccumulateLength {
     var length = 0
-    override def doOutput(result: (String, Int)): Unit = { length += 1 }
+    override def doOutput(result: (Int, Int)): Unit = { length += 1 }
   }
 
   val sizes = Gen.exponential("size")(1000, 10000000, 10)
+  val windows = Gen.exponential("windows")(1000, 10000000, 10)
+  var windowArray = Array[Int](1, 5, 10)
 
   performance of "CumulativeLengthImperative" in {
     measure method "run" in {
       using(sizes) in {
-        size => sut.run(Iterator.fill(size)("hello"))
+        size => sut.run(Iterator.fill(size)(1), windowArray)
       }
     }
   }
